257082d4f7e33a33bd0563f7c08899f7c4f3c33c
dario <dacrumau@gmail.com>
2023-08-17 13:38:23 +0200

    indogosa

---
+++ custom_indogosa/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Custom Indogosa",
+    "summary": "Adds cost field of Materials and its components.",
+    "version": "16.0.1.0.0",
+    "category": "Manufacture",
+    "author": "Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": ["mrp"],
+    "data": [
+        "views/mrp_view.xml",
+        "report/mrp_report_bom_structure_inherit.xml"
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'custom_indogosa/static/src/js/formatters.js',
+            ]
+    },
+    "installable": True,
+}

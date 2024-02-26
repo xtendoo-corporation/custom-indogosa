ef3910e0930228bc7273262da99ed38b4000ce92
dacrumau <dacrumau@gmail.com>
2024-02-26 11:01:00 +0100

    website_fomat

---
+++ document_format_indogosa/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Document Format Indogosa",
+    "summary": """Formatos de documentos de Indogosa""",
+    "version": "16.0.1.0.0",
+    "description": """Formatos de documentos de Indogosa""",
+    "author": "Darío Cruz",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "application": True,
+    "depends": [
+        "base",
+        "web"
+    ],
+    "data": [
+        "views/paper_format.xml",
+        "views/sale_order/report_sale_document.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ website_format/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website format",
+    "version": "16.0.2.0.0",
+    "category": "Website",
+    "author": "Xtendoo - Darío Cruz Mauro",
+    "license": "AGPL-3",
+    "summary": "Custom website format",
+    "depends": ["website_sale"],
+    "data": [
+        "views/website_sale_template.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
+
